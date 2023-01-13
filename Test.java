7a9a8ab4cf3ffe6c397862e5fcb0dec9445161bb
Yidan Wang <97656451+wyd333@users.noreply.github.com>
2023-01-13 22:34:47 +0800

    2023年1月13日的Java代码练习

--- Test.java
+++ Test.java
@@ -1,35 +1,21 @@
+import java.util.Scanner;
+
 /**
  * Created with IntelliJ IDEA.
  * Description:
  * User: 12569
- * Date: 2023-01-10
- * Time: 14:59
+ * Date: 2023-01-13
+ * Time: 12:56
  */
 public class Test {
     public static void main(String[] args) {
-        SingleList list = new SingleList();
-        list.createLinkedList();
-        list.display();
-        System.out.println(list.size());
-        list.addFirst(56);
-        list.display();
-        System.out.println(list.size());
-        list.addLast(888);
-        list.display();
-        try {
-            list.addIndex(0, 56);
-            list.display();
-        }catch (IndexOutOfBoundException e) {
-            e.printStackTrace();
-        }
-        System.out.println(list.contains(28));
-        list.removeAllKey(56);
-        list.display();
-        list.remove(67);
-        list.remove(45);
-        list.remove(34);
-        list.display();
-        list.reverse();
-        list.display();
+        Scanner reader = new Scanner(System.in);
+        double s = reader.nextDouble();
+        System.out.println(getArea(s));
+    }
+    public static double getArea(double s){
+        double x1 = 6*Math.pow(s,2);
+        double x2 = 4*Math.tan(Math.PI/6);
+        return x1/x2;
     }
 }
