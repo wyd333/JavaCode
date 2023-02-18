0099281f59ba935655992fde6d70a77dd4446360
Yidan Wang <97656451+wyd333@users.noreply.github.com>
2023-02-18 17:51:11 +0800

    2023年2月18日的Java代码练习

--- Test.java
+++ Test.java
@@ -2,19 +2,25 @@
  * Created with IntelliJ IDEA.
  * Description:
  * User: 12569
- * Date: 2023-02-17
- * Time: 19:32
+ * Date: 2023-02-18
+ * Time: 10:06
  */
 public class Test {
     public static void main(String[] args) {
-        PriorityQueue priorityQueue = new PriorityQueue();
-        int[] array = new int[]{27,15,19,18,28,34,65,49,25,37};
-        priorityQueue.createHeap(array);
-        priorityQueue.print();
-        priorityQueue.push(80);
-        priorityQueue.print();
-        priorityQueue.pollHeap();
-        priorityQueue.print();
-        System.out.println(priorityQueue.peekHeap());
+/*        System.out.println(-7/2);
+        System.out.println(7/2);*/
+        //判断素数
+        for(int num = 2; num <= 50; num++) {
+            boolean flag = true;
+            for(int i = 2; i <= Math.sqrt(num); i++) {
+                if(num % i == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag) {
+                System.out.print(num + " ");
+            }
+        }
     }
 }
